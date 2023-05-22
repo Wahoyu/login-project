4b855013be2273535d6a885b59858f4063c4116c
wahoyu <645702818@qq.com>
2023-05-22 13:43:42 +0800

    Git学习完成,开始做后端了

---
+++ login-project-backend/src/main/java/com/example/LoginProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginProjectBackendApplication.class, args);
+	}
+
+}
