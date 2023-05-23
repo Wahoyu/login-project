ef044ba3ac776dd012b75c471aa7768a38acacff
wahoyu <645702818@qq.com>
2023-05-24 00:54:00 +0800

    前端实现路由

--- study-project-frontend/src/router/index.js
+++ study-project-frontend/src/router/index.js
@@ -2,7 +2,25 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+      {
+          //配置默认路由,用<router-view>就能够接收到
+          path: '/',
+          name: 'welcome',
+          component: () => import('@/views/WelcomePage.vue'),
+          children: [{
+              path: '',
+              name: 'welcome-login',
+              component: () => import('@/components/welcome/LoginPage.vue')
+          }]
+     }, {
+          //主页路由
+          path: '/index',
+          name: 'index',
+          component:() => import('@/views/IndexPage.vue')
+
+      }
+  ]
 })
 
 export default router
---
+++ study-project-frontend/src/views/WelcomePage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div style="width: 100vw; height:100vh;overflow: hidden;display: flex">
+    <!--左侧背景图-->
+    <div style="flex: 1">
+      <el-image style="width: 100%;height: 100%;" fit="cover"
+                src="https://img1.baidu.com/it/u=4097856652,4033702227&fm=253&fmt=auto&app=120&f=JPEG?w=1422&h=800"/>
+    </div>
+    <!--左侧文字-->
+    <div class="welcome-title">
+      <div style="font-size: 30px;font-weight: bold">欢迎来到我们的学习平台</div>
+      <div style="margin-top: 10px">在这里你可以学习如何使用Java,如何搭建网站,并且与Java之父汤文龙密切交流</div>
+      <div style="margin-top: 10px">在这里你可以唱、跳、打篮球 做任何你喜欢的事</div>
+    </div>
+
+    <!--右侧登录界面-->
+    <div style="width: 350px;background-color: white;">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.welcome-title{
+  position: absolute;
+  bottom: 30px;
+  left: 30px;
+  color: white;
+  text-shadow: 0 0 10px black;
+}
+</style>
