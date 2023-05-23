c2e4e85fb65f0a3685bc33dc3394615165847f41
wahoyu <645702818@qq.com>
2023-05-23 20:29:01 +0800

    前端背景图操作成功

--- study-project-frontend/src/App.vue
+++ study-project-frontend/src/App.vue
@@ -3,10 +3,15 @@
 
 <template>
   <div style="width: 100vw; height:100vh;overflow: hidden;display: flex">
-    <div style="flex: 1;background-color: dodgerblue;">
 
+    <!--左侧背景图-->
+    <div style="flex: 1">
+      <el-image style="width: 100%;height: 100%;" fit="cover"
+                src="https://img1.baidu.com/it/u=4097856652,4033702227&fm=253&fmt=auto&app=120&f=JPEG?w=1422&h=800"/>
     </div>
-    <div style="width: 300px;background-color: red;">
+
+    <!--右侧主要操作界面-->
+    <div style="width: 400px;background-color: white;">
 
     </div>
   </div>
